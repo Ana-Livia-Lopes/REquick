6aca8a8b3ba2ebdc0c0a01ca0a8f60554ddd4240
Victor Koba <victorkoba08@gmail.com>
2026-04-28 10:34:28 -0300

    subindo logo correta e alterada na navbar, arquivo de novo escopo com a base feita

---
+++ php/novo_escopo.php
@@ -0,0 +1,52 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Requick - Escopo Inicial</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/novo_escopo.css">
+    <link href="https://fonts.googleapis.com/css2?family=Sora:wght@600;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+</head>
+<body>
+    <div class="BarraLateral">
+        <div class="LogoTopo">
+            <img src="../img/logo-requick.png" alt="Requick" class="ImagemLogo" />
+        </div>
+        <nav class="MenuNav">
+            <a href="#" class="ItemMenu ItemMenuAtivo">
+            <i class="fa-solid fa-layer-group"></i>
+                Dashboard
+            </a>
+            <a href="#" class="ItemMenu">
+            <i class="fa-solid fa-folder-open" style="color: rgb(255, 255, 255);"></i>
+                Projetos
+            </a>
+        </nav>
+        <div class="PerfilUsuario">
+            <div class="AvatarPerfil">VK</div>
+            <div class="InfoPerfil">
+                <p class="NomeUsuario">Victor Koba</p>
+                <p class="CargoUsuario">(administrador)</p>
+            </div>
+        </div>
+    </div>
+        <div class="div-container-escopo">
+            <div class="div-flecha">
+                <a href="projeto.php" class="referencia-projeto">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="currentColor" d="m12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/></svg>
+                    <p>Ir à tela inicial do projeto</p>
+                </a>
+            </div>
+            <div class="div-titulo-subtitulo-escopo">
+                <h1 class="titulo-escopo">Escopo Inicial</h1>
+                <h3 class="subtitulo-escopo">Centralize aqui as percepções e necessidades coletadas durante as entrevistas com o cliente. Este registro servirá como base estratégica para a estruturação de requisitos e consultas futuras durante todo o ciclo de vida do projeto</h3>
+            </div>
+        </div>
+</body>
+</html>
